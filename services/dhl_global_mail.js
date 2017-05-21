27d4b975a40a1d84aaa2465f5947a7bf8c45ae59
Donald C. Kirker <donald.kirker@openmobl.com>
2017-05-21 06:17:36 -0700

    Update DHL Global Express

--- services/dhl_global_mail.js
+++ services/dhl_global_mail.js
@@ -22,7 +22,7 @@ DHLGM.prototype.init = function(id, callbackStatus, callbackDetails, callbackMet
 };
 
 DHLGM.prototype.getTrackingUrl = function() {
-	return "http://webtrack.dhlglobalmail.com/?trackingnumber=" + this.id;
+	return "https://webtrack.dhlglobalmail.com/?trackingnumber=" + this.id;
 }
 
 DHLGM.prototype.getDetails = function() {
@@ -37,47 +37,85 @@ DHLGM.prototype.getDetails = function() {
 
 DHLGM.prototype.getDetailsRequestSuccess = function(response) {
 	var responseText = response.responseText;
-	
+
+Mojo.Log.info("DHL GM responseText = " + responseText);
+
 	var status = 0;
-	if(responseText.split("pickedup.png").length > 1) {
+	if (responseText.indexOf("<h2>Picked Up</h2>") != -1) { // Need confirm
 		status = 1;
-	}
-	if(responseText.split("processed.png").length > 1) {
+	} else if (responseText.indexOf("<h2>Processed</h2>") != -1) { // Need confirm
 		status = 2;
-	}
-	if(responseText.split("in_transit.png").length > 1) {
+	} else if (responseText.indexOf("<h2>En Route</h2>") != -1) {
 		status = 3;
-	}
-	if(responseText.split("tendered.png").length > 1) {
+	} else if (responseText.indexOf("<h2>Out For Delivery</h2>") != -1) { // Need confirm
 		status = 4;
-	}
-	if(responseText.split("delivered.png").length > 1) {
+	} else if (responseText.indexOf("<h2>Delivered</h2>") != -1) { // Need confirm
 		status = 5;
-	}
-	if(responseText.split("No results for your search.").length > 1) {
+	} else if (responseText.indexOf("<h4>No Results</h4>") != -1) {
 		status = -1;
 	}
-
+Mojo.Log.info("DHL GM Status: " + status);
 	this.callbackStatus(status);
 
 	if(status > 0) {
+		var metadata = {};
+		var deliveryParts = responseText.split("<h4>Estimated Delivery");
+
+		if (deliveryParts.length > 1) {
+			var deliveryStr = deliveryParts[1].split("</h4>")[1].split("<p>")[1].split("</p>")[0];
+			metadata.delivery = deliveryStr;
+Mojo.Log.info("DHL GM Delivery: " + deliveryStr);
+		}
+
+		var serviceParts = responseText.split("<dt>Service</dt>");
+
+		if (serviceParts.length > 1) {
+			metadata.serviceclass = serviceParts[1].split("<dd>")[1].split("</dd>")[0];
+Mojo.Log.info("DHL GM Service: " + metadata.serviceclass);
+		}
+
+		if (metadata != {}) {
+			this.callbackMetadata(metadata);
+		}
+
+		var activityFrag = responseText.split("Activity</h4>")[1];
 		var details = [];
-		var details2 = responseText.split("<tr class=");
+		var eventDateStr = activityFrag.split("<li class=\"timeline-date\">")[1].split("</li>")[0];
+		var details2 = activityFrag.split("<li class=\"timeline-event");
+Mojo.Log.info("DHL GM eventDateStr = " + eventDateStr);
+
 		for (var i=1; i<details2.length; i++) {
-			var tmpDate = details2[i].split("</td>")[0];
-			tmpDate = tmpDate.split(">")[2];
-			var tmpTime = details2[i].split("</td>")[1];
-			tmpTime = tmpTime.split(">")[1];
-			var tmpLoc = details2[i].split("</td>")[2];
-			tmpLoc = tmpLoc.split(">")[1];
-			var tmpNotes = details2[i].split("</td>")[3];
-			tmpNotes = tmpNotes.split(">")[1];
-			details.push({date: tmpDate + " " + tmpTime, location: tmpLoc, notes: tmpNotes});
+			var tmpTimeFrag = details2[i].split("timeline-time\"><em>")[1];
+			var tmpTime = tmpTimeFrag.split("</em>")[0] + " " + tmpTimeFrag.split("</em>")[1].split("</div>")[0];
+Mojo.Log.info("DHL GM tmpTime = " + tmpTime);
+			var tmpDate = eventDateStr + " " + tmpTime;
+Mojo.Log.info("DHL GM tmpDate = " + tmpDate);
+			var tmpLoc = details2[i].split("<div class=\"timeline-location\">")[1].split("</div>")[0].trim();
+Mojo.Log.info("DHL GM tmpLoc = " + tmpLoc);
+			var tmpNotes = details2[i].split("<div class=\"timeline-description\">")[1].split("</div>")[0];
+Mojo.Log.info("DHL GM tmpNotes = " + tmpNotes);
+
+			details.push({date: tmpDate, location: tmpLoc, notes: tmpNotes});
+
+			var eventDateFrag = details2[i].split("<li class=\"timeline-date\">");
+			if (eventDateFrag.length > 1) {
+				eventDateStr = eventDateFrag[1].split("</li>")[0];
+Mojo.Log.info("DHL GM eventDateStr = " + eventDateStr);
+			}
 		}
 		
 		//details = details.reverse();
 		
 		this.callbackDetails(details.clone());	
+	} else if (status == -1) {
+		var details = [];
+		var errorText = responseText.split("<h4>No Results</h4>")[1].split("<p>")[1].split("</p>")[0].trim();
+
+		var dateTodayString = Mojo.Format.formatDate(new Date(), {date: "short", time: "short"});
+Mojo.Log.info("DHL GM error: " + errorText);
+		details.push({date: dateTodayString, location: "", notes: errorText});
+		this.callbackStatus(0);
+		this.callbackDetails(details.clone());
 	}
 };
 
